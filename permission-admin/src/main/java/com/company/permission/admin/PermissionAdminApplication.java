6663203242c22ae7d43bb3d5d3e96ba8e9af1db7
candylove <candylove_2023@163.com>
2024-11-27 16:40:13 +0800

    architecture preparation

---
+++ permission-admin/src/main/java/com/company/permission/admin/PermissionAdminApplication.java
@@ -0,0 +1,13 @@
+package com.company.permission.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionAdminApplication.class, args);
+    }
+
+}
---
+++ permission-admin/src/test/java/com/company/permission/admin/PermissionAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.permission.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
